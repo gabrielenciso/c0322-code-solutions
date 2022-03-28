698e00dcf7131a854d4ea3f3685d8d1e4fed4ef0
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-03-28 20:15:30 +0000

    added more explanations for pseudocode

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,6 +2,11 @@
 
 // same logic as pick but if keys dont include key then push
 
+// func def
+// do for in through source obj
+// if keys .includes key is false then push to empty obj
+// return
+
 function omit(source, keys) {
   var obj = {};
   for (var key in source) {
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,6 +3,7 @@
 // func def
 // do for in through source obj
 // if keys .includes key push to empty obj
+// do an if cond for edge case of object val being undefined and if it is then delete it if previously added
 
 function pick(source, keys) {
   var obj = {};
