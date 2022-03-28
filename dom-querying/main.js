54a45fa512ba72758fd11c3cff6db1da9ac2d5d7
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-03-28 17:56:44 +0000

    added console log labels

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,19 +1,19 @@
 console.log('hello, world!');
 
 var $heading = document.querySelector('h1');
-console.log($heading);
+console.log('$heading', $heading);
 console.dir($heading);
 
 var $explanation = document.querySelector('#explanation');
-console.log($explanation);
+console.log('$explanation', $explanation);
 console.dir($explanation);
 
 var $hint = document.querySelector('.hint');
-console.log($hint);
+console.log('$hint', $hint);
 console.dir($hint);
 
 var $paragraphs = document.querySelectorAll('p');
-console.log($paragraphs);
+console.log('$paragraphs', $paragraphs);
 
 var $links = document.querySelectorAll('.example-link');
-console.log($links);
+console.log('$links', $links);
