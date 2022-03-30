926bd244f7ebefceb4645f61a8e3c9441274a890
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-03-30 21:38:39 +0000

    completed string-manipulation-3!

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -31,11 +31,41 @@ function titleCase(title) {
       continue;
     } else if (i === 0) {
       current = word[i].charAt(0).toUpperCase() + word[i].slice(1);
-      titleCased.push(current);
-      continue;
+      var pushed = false;
+      for (var k = 0; k < current.length; k++) {
+        if (punc.includes(current.charAt(k))) {
+          if (current.slice(0, k) === 'Javascript') {
+            titleCased.push('JavaScript' + current.charAt(k));
+            pushed = true;
+            continue;
+          }
+        }
+      }
+      if (!(pushed)) {
+        titleCased.push(current);
+        continue;
+      }
+
     } else if (word[i].length >= 4) {
       current = word[i].charAt(0).toUpperCase() + word[i].slice(1);
-      titleCased.push(current);
+      pushed = false;
+      for (var j = 0; j < current.length; j++) {
+        if (punc.includes(current.charAt(j))) {
+          if (current.slice(0, j) === 'Javascript') {
+            titleCased.push('JavaScript' + current.charAt(j));
+            pushed = true;
+            continue;
+          }
+
+          current = current.slice(0, j + 1) + current.charAt(j + 1).toUpperCase() + current.slice(j + 2);
+          titleCased.push(current);
+          pushed = true;
+          continue;
+        }
+      }
+      if (!(pushed)) {
+        titleCased.push(current);
+      }
     } else if (punc.includes(word[i - 1].charAt(word[i - 1].length - 1))) {
       current = word[i].charAt(0).toUpperCase() + word[i].slice(1);
       titleCased.push(current);
