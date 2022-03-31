7fa8503bbf04f09ddf612c6dca27c5e310714b06
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-03-31 00:31:39 +0000

    added file

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,13 @@
 /* exported difference */
+
+// func def
+// make difference empty arr
+// loop thru each first arr value and see if that value is included in second
+// if not then push in empty arr
+// loop thru second arr and see if values is in first
+// if not then push in empty arr
+// return
+
+// function difference(first, second) {
+//   var diff = [];
+// }
