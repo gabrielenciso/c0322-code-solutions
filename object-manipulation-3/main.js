9c6ef15117214379a4a919b2c1cd5902b26d0eb7
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-04 01:38:01 +0000

    completed object-manipulation-3!

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -2,6 +2,10 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 // function to make players
 // run prompt in console to make players
+// make empty players array
+// push empty arrays with num players;
+// loop thru players array --> make prompt asking name then make key value pair for name prop and value
+// make hand prop and null value
 
 // function to make card deck
 // make array with ranks
@@ -18,24 +22,39 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 // and assign random index to temp
 // and increment last index -1
 
-// function for dealing cards to players with player objects and card deck obj parameter & find high score
+// function for dealing cards to players
 // lodash chunk to chunk cards array into 2 to assign to hand property of players
 // loop thru players array
 // assign corresponding index of player and cards
-//
-// find high score
-// loop thru players and add up their values
-// whoever is highest store to current highest
-// if statement if whover being compared is higher score then change value
-// return name of person
+
+// score player hands
+// loop thru players with hands
+// loop thru current player hand
+// make score var
+// conditionals for values A, face cards, and reg number values
+// make score property in player and assign score
+// return players
+
+// get winner
+// loop through players and get scores
+// sort thru scores and get largest value at end index
+// for loop through players and check score value and see if equal to largest value
+// store to a winners array
+// if winners array is > 1 then we run tie breaker with winners array
+// if not then return winner
+
+// for tie breaker
+// deal cards again to players that tied
+// score player hands
+// then call play function again
 
 function makePlayers() {
-  var players = [
-    {},
-    {},
-    {},
-    {}
-  ];
+  var numPlayers = parseInt(prompt('How many players?'));
+  var players = [];
+  for (var j = 0; j < numPlayers; j++) {
+    players.push({});
+  }
+
   var name = '';
   for (var i = 0; i < players.length; i++) {
     name = prompt('Player ' + (i + 1) + ' Name: ');
@@ -70,11 +89,11 @@ function shuffle(cards) {
   return shuffled;
 }
 
-function assign() {
-  var players = makePlayers();
+function dealCards(players) {
   var cards = shuffle(cardDeck());
 
-  var chunks = _.chunk(cards, 2);
+  var numCardPerHand = parseInt(prompt('How Many Cards Per Hand?'));
+  var chunks = _.chunk(cards, numCardPerHand);
 
   for (var i = 0; i < players.length; i++) {
     players[i].hand = chunks[i];
@@ -83,7 +102,78 @@ function assign() {
   return players;
 }
 
-function play() {
-  var players = assign();
-
+function score(playersWithCards) {
+  var players = playersWithCards;
+  for (var i = 0; i < players.length; i++) {
+    var hand = players[i].hand;
+    var score = 0;
+    for (var j = 0; j < hand.length; j++) {
+      for (var key in hand[j]) {
+        if (hand[j][key] === 'A') {
+          score += 11;
+        } else if (hand[j][key] === 'J' || hand[j][key] === 'Q' || hand[j][key] === 'K') {
+          score += 10;
+        } else {
+          score += hand[j][key];
+        }
+      }
+      players[i].score = score;
+    }
+  }
+  return players;
 }
+
+function play(playersWithScore) {
+  var scores = [];
+  var winners = [];
+  for (var i = 0; i < playersWithScore.length; i++) {
+    scores.push(playersWithScore[i].score);
+    playersWithScore[i].winner = false;
+  }
+  scores.sort();
+  var highestScore = scores[scores.length - 1];
+  for (var j = 0; j < playersWithScore.length; j++) {
+    if (playersWithScore[j].score === highestScore) {
+      winners.push(playersWithScore[j]);
+      playersWithScore[j].winner = true;
+    }
+  }
+
+  if (winners.length > 1) {
+    console.log('tie: ', winners);
+    tieBreaker(winners);
+  } else {
+    console.log('winner: ', winners[0]);
+    return winners[0];
+  }
+}
+
+// Optional #7
+// function play2(players, numCards) {
+//   var playersList = players;
+//   var handNum = numCards;
+//   var cards = shuffle(cardDeck());
+
+//   var chunks = _.chunk(cards, handNum);
+
+//   for (var i = 0; i < playersList.length; i++) {
+//     playersList[i].hand = chunks[i];
+//   }
+
+//   var playersScored = score(playersList);
+//   var winner = play(playersScored);
+//   return winner;
+// }
+
+// Optional #8
+function tieBreaker(players) {
+  prompt('There is a tie! Press ok to continue');
+  var dealPlayers = dealCards(players);
+  var scorePlayers = score(dealPlayers);
+  return play(scorePlayers);
+}
+
+var players = makePlayers();
+var dealPlayers = dealCards(players);
+var scorePlayers = score(dealPlayers);
+play(scorePlayers);
