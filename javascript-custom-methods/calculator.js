4d3bc3cd2edf9e057cc04fc6184ee23eca9e1d0c
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-11 17:39:50 +0000

    completed JS custom methods

---
+++ javascript-custom-methods/calculator.js
@@ -0,0 +1,30 @@
+/* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x ** 2;
+  },
+  sumAll: function (numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  },
+  getAverage: function (numbers) {
+    var numLength = numbers.length;
+    return this.sumAll(numbers) / numLength;
+  }
+};
