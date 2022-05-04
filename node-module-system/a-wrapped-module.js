7d620d39171b03fe3ff0e30a3ad13367303fffc3
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-05-04 00:11:12 +0000

    completed node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,10 @@
+console.log('exports type: ', typeof exports);
+console.log('exports: ', exports);
+console.log('require type: ', typeof require);
+console.log('require: ', require);
+console.log('module type: ', typeof module);
+console.log('module: ', module);
+console.log('_filename type: ', typeof __filename);
+console.log('_filename: ', __filename);
+console.log('__dirname type: ', typeof __dirname);
+console.log('__dirname: ', __dirname);
