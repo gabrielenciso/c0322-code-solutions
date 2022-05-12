c0454a14caa7397c0f8e978cb6595c1154affe88
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-05-12 18:20:31 +0000

    complete promises

---
+++ es6-promises/index.js
@@ -0,0 +1,15 @@
+const takeAChance = require('./take-a-chance');
+
+const chance = takeAChance('Gabe');
+
+// chance.then(resolve => {
+//   console.log(resolve);
+// }).catch(reject => {
+//   console.error(reject.message);
+// });
+
+chance.then(resolve => {
+  console.log(resolve);
+}, reject => {
+  console.error(reject.message);
+});
