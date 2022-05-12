72314e4de076959a15e0945b6d6873f9b7441e83
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-05-12 18:23:29 +0000

    changed solution

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -2,14 +2,14 @@ const takeAChance = require('./take-a-chance');
 
 const chance = takeAChance('Gabe');
 
-// chance.then(resolve => {
-//   console.log(resolve);
-// }).catch(reject => {
-//   console.error(reject.message);
-// });
-
 chance.then(resolve => {
   console.log(resolve);
-}, reject => {
+}).catch(reject => {
   console.error(reject.message);
 });
+
+// chance.then(resolve => {
+//   console.log(resolve);
+// }, reject => {
+//   console.error(reject.message);
+// });
