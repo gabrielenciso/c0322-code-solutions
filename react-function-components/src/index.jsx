57c5d6e3830be3edb8c3699f27909837ad3bb7f3
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-05-17 19:44:38 +0000

    completed react function components

---
+++ react-function-components/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton() {
+  return (
+    <button>Click Me!</button>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+const element = <CustomButton/>;
+root.render(element);
