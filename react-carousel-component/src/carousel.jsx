5f76f0368439657e9a829cdcae083b154b618935
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-05-23 06:34:46 +0000

    finished scroll and click on dots -- need to do autoscroll function

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function Images({ images, index }) {
-  console.log(images);
   return (
     <div className="image">
       <img src={images[index]}></img>
@@ -16,14 +15,33 @@ class Carousel extends React.Component {
       autoScroll: true,
       index: 0
     };
+
+    this.handleArrowClick = this.handleArrowClick.bind(this);
+    this.handleDotScroll = this.handleDotScroll.bind(this);
   }
 
-  handleClick() {
-
+  handleArrowClick(event) {
+    const arrowDirection = event.target.getAttribute('id');
+    const index = this.state.index;
+    if (arrowDirection === 'left') {
+      if (index === 0) {
+        this.setState({ index: (this.props.images.length - 1) });
+      } else {
+        this.setState({ index: index - 1 });
+      }
+    } else if (arrowDirection === 'right') {
+      if (index === (this.props.images.length - 1)) {
+        this.setState({ index: 0 });
+      } else {
+        this.setState({ index: index + 1 });
+      }
+    }
   }
 
-  handleScroll() {
-
+  handleDotScroll(event) {
+    const dataIndex = event.target.getAttribute('data-index');
+    this.setState({ index: parseInt(dataIndex) });
+    console.log(dataIndex);
   }
 
   handleAutoScroll() {
@@ -32,20 +50,29 @@ class Carousel extends React.Component {
 
   render() {
     const { images } = this.props;
-    const dots = images.map((val, index) =>
-      <span data-index={index} key={index}>
-        <i className="fa-regular fa-circle fa-lg"></i>
-      </span>
+    const dots = images.map((val, index) => {
+      let className;
+      if (index === this.state.index) {
+        className = 'fa-regular fa-circle fa-lg dot-highlight';
+      } else {
+        className = 'fa-regular fa-circle fa-lg';
+      }
+      return (
+        <span key={index}>
+          <i data-index={index} className={className} onClick={this.handleDotScroll}></i>
+        </span>
+      );
+    }
     );
     return (
       <div className="container">
         <div className='image-scroll'>
           <div className="left">
-            <i className="fa-solid fa-caret-left fa-2xl"></i>
+            <i id="left" className="fa-solid fa-caret-left fa-2xl" onClick={this.handleArrowClick}></i>
           </div>
           <Images images={images} index={this.state.index}/>
           <div className='right'>
-            <i className="fa-solid fa-caret-right fa-2xl"></i>
+            <i id="right" className="fa-solid fa-caret-right fa-2xl" onClick={this.handleArrowClick}></i>
           </div>
         </div>
         <div className="row">
